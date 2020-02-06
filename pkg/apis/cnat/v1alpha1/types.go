1129294bdf2c8ab01f6789426804a6ff20551a0e
razzvan91 <razvan91@gmail.com>
2020-02-06 16:04:13 +0200

    initial commit

---
+++ pkg/apis/cnat/v1alpha1/types.go
@@ -0,0 +1,43 @@
+/*
+Copyright 2017 The Kubernetes Authors.
+
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+
+    http://www.apache.org/licenses/LICENSE-2.0
+
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
+*/
+
+package v1alpha1
+
+// +genclient
+// +k8s:deepcopy-gen:interfaces=k8s.io/apimachinery/pkg/runtime.Object
+
+const (
+	PhasePending = "PENDING"
+	PhaseRunning = "RUNNING"
+	PhaseDone    = "DONE"
+)
+
+// AtSpec defines the desired state of At
+type AtSpec struct {
+	// Schedule is the desired time the command is supposed to be executed.
+	// Note: the format used here is UTC time https://www.utctime.net
+	Schedule string `json:"schedule,omitempty"`
+	// Command is the desired command (executed in a Bash shell) to be
+	// executed.
+	Command string `json:"command,omitempty"`
+}
+
+// AtStatus defines the observed state of At
+type AtStatus struct {
+	// Phase represents the state of the schedule: until the command is
+	// executed it is PENDING, afterwards it is DONE.
+	Phase string `json:"phase,omitempty"`
+}
